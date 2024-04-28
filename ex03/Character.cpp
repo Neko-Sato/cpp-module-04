d7f9fbb8665a75b886c8713b10893d2943d00cc5
Neko-Sato <hisatosi35350@gmail.com>
2024-04-29 06:42:14 +0900

    update

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 17:34:48 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/04/26 19:58:04 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/04/29 06:41:47 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ Character::Character(std::string name) : _name(name) {
 }
 
 Character::Character(const Character &other) : _name(other._name) {
-  for (size_t i = 0; i < Character::inventory_len; i++) {
+  for (size_t i = 0; i < inventory_len; i++) {
     try {
       if (other._inventory[i])
         _inventory[i] = other._inventory[i]->clone();
@@ -34,14 +34,14 @@ Character::Character(const Character &other) : _name(other._name) {
 }
 
 Character::~Character() {
-  for (size_t i = 0; i < Character::inventory_len; i++)
+  for (size_t i = 0; i < inventory_len; i++)
     if (_inventory[i]) delete _inventory[i];
 }
 
 Character &Character::operator=(const Character &other) {
   if (this != &other) {
-    AMateria *temp[Character::inventory_len];
-    for (size_t i = 0; i < Character::inventory_len; i++) {
+    AMateria *temp[inventory_len];
+    for (size_t i = 0; i < inventory_len; i++) {
       try {
         if (other._inventory[i])
           temp[i] = other._inventory[i]->clone();
@@ -65,16 +65,16 @@ std::string const &Character::getName() const {
 
 void Character::equip(AMateria *m) {
   size_t i;
-  for (i = 0; i < Character::inventory_len; i++)
+  for (i = 0; i < inventory_len; i++)
     if (!_inventory[i]) break;
-  if (i < Character::inventory_len) _inventory[i] = m;
+  if (i < inventory_len) _inventory[i] = m;
 }
 
 void Character::unequip(int idx) {
-  if (idx < Character::inventory_len) _inventory[idx] = NULL;
+  if (idx < inventory_len) _inventory[idx] = NULL;
 }
 
 void Character::use(int idx, ICharacter &target) {
-  if (idx < Character::inventory_len && _inventory[idx])
+  if (idx < inventory_len && _inventory[idx])
     _inventory[idx]->use(target);
 }
--- ex03/MateriaSource.cpp
+++ ex03/MateriaSource.cpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 19:26:00 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/04/26 20:00:53 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/04/29 06:40:01 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ MateriaSource::MateriaSource(void) {
 }
 
 MateriaSource::MateriaSource(const MateriaSource& other) {
-  for (size_t i = 0; i < MateriaSource::inventory_len; i++) {
+  for (size_t i = 0; i < inventory_len; i++) {
     try {
       if (other._inventory[i])
         _inventory[i] = other._inventory[i]->clone();
@@ -34,14 +34,14 @@ MateriaSource::MateriaSource(const MateriaSource& other) {
 }
 
 MateriaSource::~MateriaSource() {
-  for (size_t i = 0; i < MateriaSource::inventory_len; i++)
+  for (size_t i = 0; i < inventory_len; i++)
     if (_inventory[i]) delete _inventory[i];
 }
 
 MateriaSource& MateriaSource::operator=(const MateriaSource& other) {
   if (this != &other) {
-    AMateria* temp[MateriaSource::inventory_len];
-    for (size_t i = 0; i < MateriaSource::inventory_len; i++) {
+    AMateria* temp[inventory_len];
+    for (size_t i = 0; i < inventory_len; i++) {
       try {
         if (other._inventory[i])
           temp[i] = other._inventory[i]->clone();
@@ -60,13 +60,13 @@ MateriaSource& MateriaSource::operator=(const MateriaSource& other) {
 
 void MateriaSource::learnMateria(AMateria* m) {
   size_t i;
-  for (i = 0; i < MateriaSource::inventory_len; i++)
+  for (i = 0; i < inventory_len; i++)
     if (!_inventory[i]) break;
-  if (i < MateriaSource::inventory_len) _inventory[i] = m;
+  if (i < inventory_len) _inventory[i] = m;
 }
 
 AMateria* MateriaSource::createMateria(std::string const& type) {
-  for (size_t i = 0; i < MateriaSource::inventory_len; i++) {
+  for (size_t i = 0; i < inventory_len; i++) {
     if (_inventory[i] && _inventory[i]->getType() == type)
       return _inventory[i]->clone();
   }
